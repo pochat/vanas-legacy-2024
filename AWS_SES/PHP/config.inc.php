c82d9dc84401b9a9a54aab926328444291f1c337
mikelangeljd1989 <mikel_angel_JD@hotmail.com>
2024-12-04 19:13:18 -0600

    add aws

--- AWS_SES/PHP/config.inc.php
+++ AWS_SES/PHP/config.inc.php
@@ -3,9 +3,9 @@
     'services' => array(
         'default_settings' => array(
             'params' => array(
-                'key'    => '',
-                'secret' => '',
-                'region' => ''
+                'key'    => '',//AKIAIGBBT32N6QGTEV4Q--
+                'secret' => '',//PUGXQO9D9TFVrqsBQMWHZRInyiQRR0Atqrfgjdm8--
+                'region' => ''//us-west-2--
             )
         )
     )
