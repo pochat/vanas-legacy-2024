08c6a2561ad1f54d5ca0f3a83578f1dbf4266a86
mikelangeljd1989 <mikel_angel_JD@hotmail.com>
2024-12-11 22:05:14 -0600

    Update backup_db.sh

--- cronjobs/backup_db.sh
+++ cronjobs/backup_db.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Variables
-BACKUP_DIR="/var/www/html/vanas/backup_db"
+BACKUP_DIR="/efs/vanas_uploads/backup_db"
 MYSQL_USER="vanas"
 MYSQL_PASSWORD="D#v3L0p3rr2025"
 DATABASE="vanas_prod"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
 # Realizar respaldo
-mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $DATABASE | gzip > $BACKUP_DIR/$DATABASE-$TIMESTAMP.sql.gz
+mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $DATABASE > $BACKUP_DIR/vanas_prod-$TIMESTAMP.sql
+
+mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD vanas_plus > $BACKUP_DIR/vanas_plus-$TIMESTAMP.sql
 
 # Opcional: Borrar respaldos antiguos (más de 7 días)
-find $BACKUP_DIR -name "*.sql.gz" -type f -mtime +7 -delete
+find $BACKUP_DIR -name "*.sql" -type f -mtime +7 -delete
